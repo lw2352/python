3c06f2d6bc518525e9d56b62acd05e8e3376bb77
lw2352 <2352574719@qq.com>
2016-07-24 13:32:36 +0000

    Add files via upload

--- jandan-spider.py
+++ jandan-spider.py
@@ -1,7 +1,7 @@
 import urllib.request
 import os 
-
-url = 'http://jandan.net/ooxx/'
+import time
+#url = 'http://jandan.net/ooxx/'
 
 #打开url地址，并得到html（未进行utf-8解码）
 def url_open(url): 
@@ -12,6 +12,7 @@ def url_open(url):
         })
     html = urllib.request.urlopen(req)
     html = html.read()
+    time.sleep(15)
     return html   
 
 #获得当前网页的页号
@@ -23,29 +24,56 @@ def get_page(url):
     return html[a:b]   #页号
 
  #获取图片的url地址，并将其写入文件
-def find_imgs():
+def find_imgs(url):
     html = url_open(url).decode('utf-8')
-    img_addrs = [ ]
+    img_addrs = []
     
     a = html.find('img src=')
 
     while a != -1:
         b = html.find('.jpg',a,a +255)
         if b !=-1:
-            img_addrs.append(html[a+9:b+4]) #  ？
+            img_addrs.append(html[a+9:b+4]) 
         else:
             b = a +9
             
         a = html.find('img src=',b)
         
-        for each in img_addrs:
-                dizhi_write(each)
-                dizhi_write('\n')
-                print (each)    #图片的地址
+        #for each in img_addrs:
+                #dizhi_write(each)
+                #dizhi_write('\n')
+                #print (each)    #图片的地址
+
+    return img_addrs
 
 #写入文件操作
 def dizhi_write(dizhi): 
     with open('img_addrs.txt','a') as f:
         f.write(dizhi)
+
+#自定义要下载的页数
+def download_mm(folder='OOXX',pages=10):
+    os.mkdir(folder)
+    os.chdir(folder)
+    url = 'http://jandan.net/ooxx/'
+    page_num = int(get_page(url))
+
+    for i in range(pages):
+        page_num -= i
+        page_url = url + 'page-' + str(page_num) + '#comments'
+        img_addrs = find_imgs(page_url)
+        save_imgs(folder,img_addrs)
+        for each in img_addrs:
+            dizhi_write(each)
+            dizhi_write('\n')
+            print (each)
+
+#通过图片的url地址来下载图片
+def save_imgs(folder, img_addrs):
+    for each in img_addrs:
+        filename = each.split('/')[-1]
+        with open(filename,'wb') as f:
+            img =url_open(each)
+            f.write(img)
     
-find_imgs()
+download_mm()
